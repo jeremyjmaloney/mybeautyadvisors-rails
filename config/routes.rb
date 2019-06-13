844e79b9e45f40f0fb31e415f1c8d3becd0fd7be
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-13 16:58:00 -0400

    Set up routes.rb with all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/advisors', to:'advisors#all'
+
+  get '/advisors/:id', to:'advisors#show'
+
+  post '/advisors', to:'advisors#create'
+
+  delete '/advisors/:id', to:'advisors#delete'
+
+  get '/weeks/:id', to:'weeks#show'
+
+  get '/weeks/:id/:date', to:'weeks#find'
+
+  post '/weeks', to:'weeks#create'
+
+  delete '/weeks/:id/:date', to:'weeks#delete'
+
 end
